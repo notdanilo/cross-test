aa35e15ead7eae6e1a4dc119001a7f00319127f5
Danilo Guanabara <danguafer@gmail.com>
2021-01-13 17:49:07 -0300

    Re-exporting web_configure macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,24 @@
 #[cfg(not(target_arch = "wasm32"))]
 mod platform {
     pub use tokio::test as test;
+
+    #[macro_export]
+    macro_rules! configure {
+        () => {}
+    }
 }
 
 #[cfg(target_arch = "wasm32")]
 mod platform {
     pub use wasm_bindgen_test::wasm_bindgen_test_configure as web_configure;
     pub use wasm_bindgen_test::wasm_bindgen_test as test;
-    web_configure!(run_in_browser);
+
+    #[macro_export]
+    macro_rules! configure {
+        () => {
+            $crate::web_configure!(run_in_browser);
+        }
+    }
 }
 
 pub use platform::*;
