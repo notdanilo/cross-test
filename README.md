c164310d7b9b0c6fc568dc2f456a9189fa8f8852
Danilo Guanabara <danguafer@gmail.com>
2021-01-27 18:41:15 -0300

    Updated docs

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # cross-test
-Rust native and web testing in a single framework
+Run Rust native and web tests with a single framework.
 
 ### Platforms
 * Web: web-sys
 * Native: tokio
+
+ ### Example
+ ```rust
+use cross_test::prelude::*;
+
+cross_test_configure!();
+
+#[cross_test]
+async fn it_works() {
+    assert_eq!(2 + 2, 4);
+}
+ ```
+
+### Important issue
+
+Because `#[cross_test]` gets translated to `#[tokio::test]` all the tests must be `async`.
+
+A custom proc-macro will be provided to select the test executor if the `async` work is present or not.
+
+[Link for the issue: https://github.com/notdanilo/cross-test/issues/1](https://github.com/notdanilo/cross-test/issues/1)
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,41 @@
+//! # cross-test
+//! Run Rust native and web tests with a single framework.
+//!
+//! ### Platforms
+//! * Web: web-sys
+//! * Native: tokio
+//!
+//! ### Requirements
+//!
+//! To run the `wasm-bindgen-test`s, you need to have the nightly toolchain installed (there is no need to make it default).
+//!
+//! You will also need to manually include `wasm-bindgen-test` as a dev dependency in your project because `#[wasm_bindgen_test]` macro can't be re-exported to avoid this requirement because of how it was designed.
+//!
+//! ```
+//! [target.'cfg(target_arch = "wasm32")'.dependencies]
+//! wasm-bindgen-test = "0.3.20"
+//! ```
+//!
+//! ### Usage
+//! ```
+//!use cross_test::prelude::*;
+//!
+//!cross_test_configure!();
+//!
+//!#[cross_test]
+//!async fn it_works() {
+//!    assert_eq!(2 + 2, 4);
+//!}
+//! ```
+//!
+//! ### Important issue
+//!
+//! Because `#[cross_test]` gets translated to `#[tokio::test]` all the tests must be `async`.
+//!
+//! A custom proc-macro will be provided to select the test executor if the `async` work is present or not.
+
+#[warn(missing_docs)]
+
 pub mod prelude;
 
 #[cfg(not(target_arch = "wasm32"))]
